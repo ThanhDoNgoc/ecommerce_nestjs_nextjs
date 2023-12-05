259cb68d73541cabd5408f5cdf54c4ae31a74098
ThanhDoNgoc <dongocthanh40@gmail.com>
2023-12-05 15:20:46 +0700

    [Add] Update & delete category

--- backend/be_products/src/categories/categories.controller.ts
+++ backend/be_products/src/categories/categories.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Controller('categories')
 export class CategoriesController {
@@ -19,5 +28,18 @@ export class CategoriesController {
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.categoriesService.findOne(+id);
+  }
+
+  @Put(':id')
+  async updateOne(
+    @Param('id') id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {
+    return await this.categoriesService.update(+id, updateCategoryDto, 'test');
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return await this.categoriesService.delete(+id);
   }
 }
