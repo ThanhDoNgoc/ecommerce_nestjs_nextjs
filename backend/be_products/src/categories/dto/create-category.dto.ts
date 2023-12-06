bf60c29171670a7cb1b44d617f05325cfd8dd2eb
ThanhDoNgoc <dongocthanh40@gmail.com>
2023-12-06 10:32:53 +0700

    [Add] Api doc

--- backend/be_products/src/categories/dto/create-category.dto.ts
+++ backend/be_products/src/categories/dto/create-category.dto.ts
@@ -1,3 +1,6 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateCategoryDto {
-  name : string
+  @ApiProperty({type: String})
+  name: string;
 }
--- backend/be_products/src/products/products.controller.ts
+++ backend/be_products/src/products/products.controller.ts
@@ -13,32 +13,39 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { FindProductDto } from './dto/find-product.dto';
 import { UpdateProductDto } from './dto/update-products.dto';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Products } from '../entities';
 
 @Controller('products')
+@ApiTags('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiOkResponse({ type: Products })
   async create(@Body() createProductDto: CreateProductDto) {
     return await this.productsService.create(createProductDto, 'test');
   }
 
   @Get()
+  @ApiOkResponse({ type: Products, isArray: true })
   async find(@Query() findProductDto: FindProductDto) {
     return await this.productsService.find(findProductDto);
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: Products })
   async findOne(@Param('id') id: string) {
-    console.log("find by Id")
+    console.log('find by Id');
     return await this.productsService.findOne(+id);
   }
 
   @Put(':id')
+  @ApiOkResponse()
   async update(
     @Param('id') id: string,
     @Body() updateProductDto: UpdateProductDto,
   ) {
-    return await this.productsService.update(+id, updateProductDto, "test");
+    return await this.productsService.update(+id, updateProductDto, 'test');
   }
 }
