cb211d1aec9d7c776bc580159731d8f9bb1f00cc
ThanhDoNgoc <dongocthanh40@gmail.com>
2023-12-11 13:36:15 +0700

    [Mod] Api permissions, Api doc

---
+++ backend/be_products/src/authorization/permissions.ts
@@ -0,0 +1,9 @@
+export enum AuthPermissions {
+  CATEGORIES_CREATE = 'create:categories',
+  CATEGORIES_UPDATE = 'update:categories',
+  CATEGORIES_DELETE = 'delete:categories',
+
+  PRODUCTS_CREATE = 'create:products',
+  PRODUCTS_UPDATE = 'update:products',
+  PRODUCTS_DELETE = 'delete:products',
+}
