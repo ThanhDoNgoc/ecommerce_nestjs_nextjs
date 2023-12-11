6134d1e7a45eaa197ea78171d4d9704b120b202d
ThanhDoNgoc <dongocthanh40@gmail.com>
2023-12-11 13:29:11 +0700

    [Mod] Api Doc Define

--- backend/be_products/src/entities/categories.entity.ts
+++ backend/be_products/src/entities/categories.entity.ts
@@ -1,8 +1,10 @@
 import { Entity, Column } from 'typeorm';
 import { BaseEntity } from './base.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Categories extends BaseEntity {
+  @ApiProperty({type: String})
   @Column()
   name: string;
 }
