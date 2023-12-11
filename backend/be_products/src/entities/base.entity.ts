b1cf7c6d2aba13512e901a17bf2bc773f7186a7e
ThanhDoNgoc <dongocthanh40@gmail.com>
2023-12-11 13:29:11 +0700

    [Mod] Api Doc Define

--- backend/be_products/src/entities/base.entity.ts
+++ backend/be_products/src/entities/base.entity.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Column, PrimaryGeneratedColumn } from 'typeorm';
 
 export class BaseEntity {
+  @ApiProperty({type: Number})
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({type: Date})
   @Column()
   updatedAt: Date;
 
+  @ApiProperty({type: String})
   @Column()
   updatedBy: string;
 }
--- backend/be_products/src/entities/products.entity.ts
+++ backend/be_products/src/entities/products.entity.ts
@@ -1,24 +1,32 @@
 import { Entity, Column, ManyToMany, JoinTable } from 'typeorm';
 import { BaseEntity } from './base.entity';
 import { Categories } from './categories.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Products extends BaseEntity {
+
+  @ApiProperty({type: String})
   @Column()
   name: string;
 
+  @ApiProperty({type: Number})
   @Column()
   price: number;
 
+  @ApiProperty({type: String})
   @Column()
   description: string;
 
+  @ApiProperty({type: Number})
   @Column()
   inStock: number;
 
+  @ApiProperty({type: Boolean})
   @Column({ default: false })
   isPublic: boolean;
 
+  @ApiProperty({type: [Categories]})
   @ManyToMany(() => Categories)
   @JoinTable()
   categories: Categories[];
